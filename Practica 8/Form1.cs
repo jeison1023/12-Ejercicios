67805c91fcfef0e3990d5037f0fc0bdce8500f9c
Luis Alberto <yeisonpuello921@gmail.com>
2025-06-19 00:22:52 -0400

    practica ocho

---
+++ Practica 8/Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Practica_8
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            String Text = textBox1.Text.Trim();
+            string[] Palabras = textBox1.Text.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+
+            int cantida = Palabras.Length;
+
+            label1.Text = cantida.ToString();
+        }
+    }
+}
